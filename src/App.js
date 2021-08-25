e821685c1224fe1ca02d2d0fa935758ef0c2c5e4
Ankit kumar <thankitkumar@gmail.com>
2021-08-25 17:29:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Users from './components/users';
+import Pagination from './components/Pagination';
+import axios from 'axios';
+import { USER_PER_PAGE } from "./utils/constants";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  console.log(users)
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      const res = await axios.get('https://reqres.in/api/users?page=2');
+      setLoading(false);
+      setUsers(res.data.data);
+      setTotalPages(Math.ceil(res.data.data.length / USER_PER_PAGE));
+    };
+    fetchUsers();
+  }, []);
+
+  const handleClick = num => {
+    setPage(num);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Question 2. Paging</h1>
+      <p>Page {page}</p>
+      { loading ? <p>Loading...</p> : <>
+        <Users users={users} page={page} />
+        <Pagination totalPages={totalPages} handleClick={handleClick} />
+      </> }
     </div>
   );
 }
